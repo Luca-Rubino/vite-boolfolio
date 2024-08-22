2cae07b32949080300712b9ad1a4c03bb815fb2a
Luca-Rubino <lucasrubino92@gmail.com>
2024-08-22 16:25:06 +0200

    customize main.vue

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,4 @@
 <script setup>
-
 defineProps({
     props: {
         type: String,
@@ -7,11 +6,17 @@ defineProps({
     }
 })
 
+import IconCommunity from './icons/IconCommunity.vue'
+import IconDocumentation from './icons/IconDocumentation.vue'
+import IconEcosystem from './icons/IconEcosystem.vue'
+import IconSupport from './icons/IconSupport.vue'
+import IconTooling from './icons/IconTooling.vue'
 </script>
 
 <template>
     <main>
         <h2>{{ props }}</h2>
+
         <section>
             <article>
                     <h3>Title_article</h3>
@@ -19,6 +24,28 @@ defineProps({
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nisi, <a href="/">link</a> ipsa iste porro eius, explicabo cumque, neque doloremque ullam vitae nulla blanditiis amet voluptas nihil? Magnam cum veritatis ipsa. Dolor. Sed quae quibusdam quos tempore aliquam mollitia, at illum, <a href="/">link</a> deleniti consectetur assumenda odio cupiditate minima, iste ipsum recusandae harum magni officia reiciendis alias nulla doloribus aliquid impedit itaque numquam. Corporis. Dignissimos recusandae mollitia voluptates obcaecati, aspernatur <a href="/">link</a> quasi natus sunt minima accusamus! A velit nobis, est eius culpa totam? Vitae dicta porro iste id cupiditate nihil adipisci aliquid explicabo deserunt facere!
                     </p>
             </article>
+        </section>
+
+        <section>
+            <div>
+              <IconCommunity/>  
+            </div>
+            
+            <div>
+              <IconDocumentation/>  
+            </div>
+
+            <div>
+              <IconEcosystem/>  
+            </div>
+
+            <div>
+              <IconSupport/>  
+            </div>
+
+            <div>
+              <IconTooling/>  
+            </div>            
         </section>
     </main>
 </template>
@@ -43,6 +70,21 @@ main {
         a {
             color: var(--my-tx-purple);
         }
+    }
+}
+
+section:nth-of-type(2) {
+    display: flex;
+    justify-content: space-between;
+    padding: 1rem 0;
+
+    div {
+        background-color: var(--primary-color);
+        color: var(--secondary-color);
+        padding: 1rem 1rem;
+        display: flex;
+        border: 3px solid var(--my-tx-purple);
+        border-radius: 35px;
     }
 }
 </style>
