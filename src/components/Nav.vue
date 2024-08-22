fae8e9e9169e7b864973d9ac260955f3a152aab3
Luca-Rubino <lucasrubino92@gmail.com>
2024-08-22 14:10:50 +0200

    Create component Nav

---
+++ src/components/Nav.vue
@@ -0,0 +1,71 @@
+<script setup>
+defineProps({
+    props: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <nav>
+        <section>
+            <h1>
+                {{ props }}
+            </h1>
+        </section>
+
+        <section>
+            <img src="./../assets/logo.svg" alt="logo">
+            <ul>
+                <li>
+                    <a href="/">
+                        link
+                    </a>
+                </li>
+            </ul>
+        </section>
+
+    </nav>
+</template>
+
+<style lang="scss" scoped>
+nav {
+    background-color: white;
+    color: black;
+    padding: .7rem;
+
+    h1 {
+        text-align: center;
+        padding-bottom: .3rem;
+    }
+
+    img {
+        width: 3rem;
+    }
+
+    ul {
+        list-style: none;
+
+        li {
+            padding-left: .8rem;
+        }
+    }
+
+    h1 {
+        font-size: 2rem;
+        font-variant: small-caps;
+    }
+
+    a {
+        color: purple;
+        text-decoration: underline;
+    }
+
+    section:nth-of-type(2) {
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+    }
+}
+</style>
