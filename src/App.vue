1d07a8d474db23509d92b5c93a50ac3de4c646fa
Luca-Rubino <lucasrubino92@gmail.com>
2024-11-02 17:15:01 +0100

    FullReset_App

--- src/App.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <h1>
-    Home
-  </h1>
-  <p>Lorem ipsum dolor <a href="/">sit amet</a> consectetur adipisicing.</p>
-</template>
--- src/main.js
+++
@@ -1,7 +0,0 @@
-import './assets/app.scss';
-import App from './App.vue';
-
-import { createApp } from 'vue';
-import { router } from './router';
-
-createApp(App).use(router).mount('#app')
